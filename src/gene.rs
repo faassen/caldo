3d8d5deee4457db25efc80e11695fb15bfe0c0a4
Martijn Faassen <faassen@startifact.com>
2019-09-26 22:22:17 +0200

    Better handling of add errors.

--- src/gene.rs
+++ src/gene.rs
@@ -18,34 +18,66 @@ impl Gene {
             return;
         }
         let instruction = lookup.find(t);
+        instruction.execute(&mut self.stack);
+        self.pc += 1;
     }
 }
 
-#[derive(Debug, PartialEq)]
-struct Instruction {
-    name: String,
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum Instruction {
+    Add,
+    Sub,
 }
 
-struct InstructionLookup<'a> {
-    tree: KdTree<f32, &'a Instruction, [f32; 3]>,
+impl Instruction {
+    fn execute(&self, stack: &mut Vec<u32>) {
+        match self {
+            Instruction::Add => {
+                let v1 = stack.pop();
+                let v2 = stack.pop();
+                match v1 {
+                    Some(v) => {}
+                    None => {}
+                }
+            }
+            Instruction::Sub => {}
+        }
+    }
 }
 
-impl<'a> InstructionLookup<'a> {
-    fn new() -> InstructionLookup<'a> {
+struct InstructionLookup {
+    tree: KdTree<f32, Instruction, [f32; 3]>,
+}
+
+#[derive(Debug, Clone, Copy)]
+struct InstructionLookupError {}
+
+type InstructionLookupAddResult = Result<(), InstructionLookupError>;
+
+impl From<ErrorKind> for InstructionLookupError {
+    fn from(error: ErrorKind) -> Self {
+        InstructionLookupError {}
+    }
+}
+
+impl InstructionLookup {
+    fn new() -> InstructionLookup {
         return InstructionLookup {
             tree: KdTree::new(3),
         };
     }
 
-    fn add(&mut self, triplet: Triplet, instruction: &'a Instruction) {
-        self.tree.add(triplet.coordinates(), instruction);
+    fn add(&mut self, triplet: Triplet, instruction: Instruction) -> InstructionLookupAddResult {
+        self.tree.add(triplet.coordinates(), instruction)?;
+        return Ok(());
     }
-    fn find(&self, t: Triplet) -> &Instruction {
+
+    fn find(&self, t: Triplet) -> Instruction {
         let v = self
             .tree
             .nearest(&t.coordinates(), 1, &squared_euclidean)
             .unwrap();
-        return v[0].1;
+        return *v[0].1;
     }
 }
 
@@ -54,32 +86,28 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_instruction_lookup_identify() {
+    fn test_instruction_lookup_identify() -> InstructionLookupAddResult {
         let mut l = InstructionLookup::new();
         let t = Triplet::from_int(0x010203);
-        let i = Instruction {
-            name: "I".to_string(),
-        };
-        l.add(t, &i);
-        assert_eq!(l.find(t), &i);
+        let i = Instruction::Add;
+        l.add(t, i)?;
+        assert_eq!(l.find(t), i);
+        return Ok(());
     }
 
     #[test]
-    fn test_instruction_lookup_near() {
+    fn test_instruction_lookup_near() -> InstructionLookupAddResult {
         let mut l = InstructionLookup::new();
         let t1 = Triplet::from_int(0x010203);
         let t2 = Triplet::from_int(0xFFFFFF);
         let tlookup = Triplet::from_int(0x010402);
 
-        let i1 = Instruction {
-            name: "I1".to_string(),
-        };
-        let i2 = Instruction {
-            name: "I2".to_string(),
-        };
-        l.add(t1, &i1);
-        l.add(t2, &i2);
-        assert_eq!(l.find(tlookup), &i1);
+        let i1 = Instruction::Add;
+        let i2 = Instruction::Sub;
+        l.add(t1, i1)?;
+        l.add(t2, i2)?;
+        assert_eq!(l.find(tlookup), i1);
+        return Ok(());
     }
 
 }
